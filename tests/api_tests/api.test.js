863274c29ed7a422f0333490ac28532e4a53e77f
emmanuel.kuye <kuyeemmanuel@rocketmail.com>
2024-05-21 12:59:15 +0100

    api_test updated

--- tests/api_tests/api.test.js
+++ tests/api_tests/api.test.js
@@ -25,21 +25,20 @@ test.describe('JSONPlaceholder API Tests', () => {
   });
 
   test('@api Get Only the Created Post by ID', async () => {
-    const response = await apiHelper.getPostById(createdPostData.id); // Use the created post ID
+    const response = await apiHelper.getPostById(createdPostData.id); 
     expect(response.status).toBe(200);
     const post = response.data;
     expect(post.id).toBe(createdPostData.id);
     expect(post.title).toBe(createdPostData.title);
-    expect(post.body).toBe(createdPostData.body);
+    expect(post.body).toBe(createdPostData.body); 
   });
 
   test('@api Replace Some Field in the Created Post with PATCH', async () => {
     const updatedPost = {
       title: 'Updated Software QA Engineer',
     };
-    const response = await apiHelper.updatePost(createdPostData.id, updatedPost); // Use the created post ID
+    const response = await apiHelper.updatePost(createdPostData.id, updatedPost);
     expect(response.status).toBe(200);
-    // Optionally, you can validate the updated post details
     const updatedResponse = await apiHelper.getPostById(createdPostData.id);
     expect(updatedResponse.status).toBe(200);
     const post = updatedResponse.data;
@@ -48,19 +47,16 @@ test.describe('JSONPlaceholder API Tests', () => {
 
 
   test('@api Delete the Created Post by ID', async () => {
-    const response = await apiHelper.deletePost(createdPostData.id); // Use the created post ID
+    const response = await apiHelper.deletePost(createdPostData.id); 
     expect(response.status).toBe(200);
-    // Optionally, you can verify that the post is deleted by trying to fetch it again
     const deletedResponse = await apiHelper.getPostById(createdPostData.id);
     expect(deletedResponse.status).toBe(404);
   });
 
   test('@api Check the Number of Posts to Ensure Integrity', async () => {
-    // Retrieve all posts again to check the total count
     const response = await apiHelper.getAllPosts();
     expect(response.status).toBe(200);
     const currentPostCount = response.data.length;
-    // Compare with the initial count
-    expect(currentPostCount).toBe(initialPostCount); // Since we created one post
+    expect(currentPostCount).toBe(initialPostCount);
   });
 });
